12872781937432eecf34eeaf2ff435185ee4a400
Shivam Sharma <enohmoroh@gmail.com>
2024-02-16 18:52:58 +0000

    Save progress: 630.1

--- insert_data.sh
+++ insert_data.sh
@@ -8,13 +8,13 @@ do
 
 #get major_id
 MAJOR_ID=$($PSQL "SELECT major_id FROM majors WHERE major='$MAJOR'")
-
+echo $MAJOR_ID
 
 #if not found
 if [[ -z $MAJOR_ID ]];
 then
 #insert major
-INSERT_MAJOR_RESULT=$($PSQL "INSERT INTO majors(major) VALUES('$MAJOR')")
+INSERT_MAJOR_RESULT=$($PSQL "INSERT INTO majors(major) VALUES('$MAJOR')" "SELECT * FROM majors")
 echo $INSERT_MAJOR_RESULT
 #get new major_id
 
