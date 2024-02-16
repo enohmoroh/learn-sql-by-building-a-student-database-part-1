77c8a771ae59357fe96d0ae489b5b3eb761b5acd
Shivam Sharma <enohmoroh@gmail.com>
2024-02-16 19:50:06 +0000

    Save progress: 900.1

--- insert_data.sh
+++ insert_data.sh
@@ -12,19 +12,19 @@ do
     MAJOR_ID=$($PSQL "SELECT major_id FROM majors WHERE major='$MAJOR'")
 
 #if not found
-    if [[ -z $MAJOR_ID ]];
-then
-#insert major
-INSERT_MAJOR_RESULT=$($PSQL "INSERT INTO majors(major) VALUES('$MAJOR')")
-if [[ $INSERT_MAJOR_RESULT == "INSERT 0 1" ]]
-then 
-echo Inserted into majors, $MAJOR
-fi
-#get new major_id
-$MAJOR_ID=$($PSQL "SELECT major_id FROM majors WHERE major='$MAJOR'")
+if [[ -z $MAJOR_ID ]];
+      then
+      #insert major
+      INSERT_MAJOR_RESULT=$($PSQL "INSERT INTO majors(major) VALUES('$MAJOR')")
+      if [[ $INSERT_MAJOR_RESULT == "INSERT 0 1" ]]
+  then 
+  echo Inserted into majors, $MAJOR
+  fi
+      #get new major_id
+      $MAJOR_ID=$($PSQL "SELECT major_id FROM majors WHERE major='$MAJOR'")
 
 
-fi
+fi  
 
 
 
@@ -39,11 +39,11 @@ then
 #insert course
 INSERT_COURSE_RESULT=$($PSQL "INSERT INTO courses(course) VALUES('$COURSE')")
 if [[ $INSERT_COURSE_RESULT == "INSERT 0 1" ]]
-then
- echo Inserted into courses, $COURSE
+    then
+    echo Inserted into courses, $COURSE
 fi
 #get new course_id
-
+COURSE_ID=$($PSQL "SELECT course_id FROM courses WHERE course='$COURSE'")
 fi
 
 #insert into majors_courses
